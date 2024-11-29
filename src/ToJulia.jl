42f01f587b55cf840c3ae3afa37e6457ec8c6ec7
aptmcl <antonio.menezes.leitao@gmail.com>
2024-11-29 22:39:55 +0000

    Added module prefix processing. Added using, begin, and let/let*. Fixed bug in calls.

--- src/ToJulia.jl
+++ src/ToJulia.jl
@@ -1,6 +1,13 @@
 tojulia(form::Any) = form
 
-tojulia(form::Symbol) = form
+tojulia(form::Symbol) = 
+  let token = string(form)
+    '.' in token ?
+      let (pkg, name) = split(token, '.')
+        Expr(:., Symbol(pkg), QuoteNode(Symbol(name)))
+      end :
+      form
+  end
 
 tojulia(form::Nil) = form
 
@@ -28,6 +35,7 @@ tojulia(::Val{:quote}, (arg,)) =
 tojulia(::Val{:(=)}, args) =
   :(==($(tojulia_vector(args)...)))
 
+# Assignments
 tojulia(::Val{:def}, (sig, init)) =
   if sig isa Symbol
     :($(tojulia(sig)) = $(tojulia(init)))
@@ -37,19 +45,43 @@ tojulia(::Val{:def}, (sig, init)) =
     error("Unknown form $(list(:def, sig, init))")
   end
 
+# Conditional expressions
 tojulia(::Val{:if}, (cond, conseq, altern)) =
   :($(tojulia(cond)) ? $(tojulia(conseq)) : $(tojulia(altern)))
 
+# Anonymous functions
 tojulia(::Val{:λ}, (params, body)) = 
   tojulia(Val{:lambda}(), (params, body))
 
 tojulia(::Val{:lambda}, (params, body)) =
-    :(($(tojulia_vector(params)...),) -> $(tojulia(body)))
+  :(($(tojulia_vector(params)...),) -> $(tojulia(body)))
 
+# Globals
+tojulia(::Val{:global}, (form, )) = 
+  :(global $(tojulia(form)))
+
+# Modules
+tojulia(::Val{:using}, (form, )) =
+  :(using $(tojulia(form)))
+
+# Blocks
+tojulia(::Val{:begin}, forms) =
+  :(begin $(tojulia_vector(forms)...) end)
+
+# Local scopes
+tojulia(::Val{:let}, (binds, body...)) =
+  :(let ($(tojulia_tuple(map(head, binds))...),) = ($(tojulia_tuple(map(head ∘ tail, binds))...),)
+    $(tojulia_vector(body)...)
+    end)
+
+tojulia(::Val{Symbol("let*")}, (binds, body...)) =
+  :(let $([:($(tojulia(head(bind))) = $(tojulia(head(tail(bind))))) for bind in binds]...)
+    $(tojulia_vector(body)...)
+    end)
 
 
 # Fallback: everything is treated as a function call.
 tojulia(::Val{op}, args) where {op} = 
-  Expr(:call, op, tojulia_vector(args)...)
+  Expr(:call, tojulia(op), tojulia_vector(args)...)
   
 
