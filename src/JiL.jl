bf0ade76f662b0687b2a4fab27a3cdc310909580
aptmcl <antonio.menezes.leitao@gmail.com>
2024-12-01 23:56:12 +0000

    Added bootstrapping steps.

--- src/JiL.jl
+++ src/JiL.jl
@@ -4,5 +4,9 @@ include("List.jl")
 include("ToJulia.jl")
 include("JiLParser.jl")
 
-__init__() = install_jil_parser()
+__init__() = begin
+  install_jil_parser() # Let's have the parser ready
+  include(joinpath(@__DIR__, "Boot0.jil")) # Defines primitive operations (car, cdr, null?, defmacro, etc)
+  include(joinpath(@__DIR__, "Boot1.jil")) # Defines useful macros that use the primitives (cond, macroexpand, etc)
+end
 end
