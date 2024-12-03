7b1030b079d7f1b8976acb428921607b6e138f47
aptmcl <antonio.menezes.leitao@gmail.com>
2024-12-03 10:40:48 +0000

    Updated tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,10 @@ using Test
 
 @testset "JiL.jl" begin
     @test jil"(def foo 1)" == julia"foo = 1"
-    @test jil"(def (bar x) 1)" == julia"bar(x) = begin 1 end"
-    @test jil"(def (baz x (... y)) 1)" == julia"baz(x, y...) = begin 1 end"
+    @test jil"(def (foo x) 1)" == julia"foo(x) = begin 1 end"
+    @test jil"(def (foo x (... y)) 1)" == julia"foo(x, y...) = begin 1 end"
+    @test jil"(def (foo x (kw y 2)) 1)" == julia"foo(x, y=2) = begin 1 end"
     @test jil"(define foo 1)" == julia"foo = 1"
-    @test jil"(define (bar x) 1)" == julia"bar(x) = begin 1 end"
-    @test jil"(define (baz x . y) 1)" == julia"baz(x, y...) = begin 1 end"
+    @test jil"(define (foo x) 1)" == julia"foo(x) = begin 1 end"
+    @test jil"(define (foo x . y) 1)" == julia"foo(x, y...) = begin 1 end"
 end
