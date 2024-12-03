03363c1b6f5047f70d373ee070eb76e47e52be26
aptmcl <antonio.menezes.leitao@gmail.com>
2024-12-03 00:46:49 +0000

    Code reorganization.

--- src/JiL.jl
+++ src/JiL.jl
@@ -6,7 +6,8 @@ include("JiLParser.jl")
 
 __init__() = begin
   install_jil_parser() # Let's have the parser ready
-  include(joinpath(@__DIR__, "Boot0.jil")) # Defines primitive operations (car, cdr, null?, defmacro, etc)
-  include(joinpath(@__DIR__, "Boot1.jil")) # Defines useful macros that use the primitives (cond, macroexpand, etc)
+  include(joinpath(@__DIR__, "Boot0.jil")) # Primitive operations (car, cdr, null?, defmacro, etc)
+  include(joinpath(@__DIR__, "Boot1.jil")) # Useful macros that use the primitives (cond, macroexpand, etc)
+  include(joinpath(@__DIR__, "Boot2.jil")) # More useful macros (when, etc)
 end
 end
