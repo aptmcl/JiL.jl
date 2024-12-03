e28f01bc1d3c65ad4a9de0c4eec6a8eeef6071db
aptmcl <antonio.menezes.leitao@gmail.com>
2024-12-03 00:44:14 +0000

    Added typed implementation for Pairs.

--- src/List.jl
+++ src/List.jl
@@ -1,43 +1,51 @@
 export List, Nil, Cons, list, cons, nil, head, tail
 
-abstract type List{T} end
+abstract type Tree{L,R} end
+
+abstract type List{T} <: Tree{T,List{T}} end
 
 struct Nil{T} <: List{T} end
 
-struct Cons{T} <: List{T}
-  head::T
-  tail::List{T}
+struct Cons{E} <: List{E}
+  head::E
+  tail::List{E}
 end
 
-Base.Pair(h::T, t::List{L}) where {T, L} = Cons{typejoin(T, L)}(h, t)
+#Base.Pair(h::T, t::List{L}) where {T, L} = Cons{typejoin(T, L)}(h, t)
 
 const nil = Nil{Union{}}()
 
-cons(h::T, t::List{T}) where {T} = Cons{T}(h, t)
-
-cons(h::T, t::List{L}) where {T, L} =
-  let Type = typejoin(T, L)
-    Cons{Type}(convert(Type, h), convert(List{Type}, t))
+#cons(h::E, t::Nil{T}) where {E,T<:E} = Cons{E}(h, Nil{E}()) Use conversion
+cons(h::F, t::List{E}) where {E,F<:E} = Cons{E}(h, t)
+cons(h::F, t::List{E}) where {E,F} =
+  let Type = typejoin(F, E)
+    Cons{Type}(h, t)
   end
 
-(::Colon)(h::T, t::List{L}) where {T, L} = cons(h, t)
+struct Pair{A,D} <: Tree{A,D}
+  car::A
+  cdr::D
+end
 
-Base.convert(::Type{List{T}}, l::Nil{Union{}}) where {T} = Nil{T}()
+cons(a::A, d::D) where {A,D} = Pair(a, d)
+
+#=
+(::Colon)(h::T, t::List{L}) where {T, L} = cons(h, t)
+=#
 Base.convert(::Type{List{T1}}, l::Nil{T2}) where {T1, T2 <: T1} = Nil{T1}()
 Base.convert(::Type{List{T1}}, l::Cons{T2}) where {T1, T2 <: T1} = Cons{T1}(l.head, l.tail)
 Base.convert(::Type{List}, t::Tuple) = list(t...)
 
-list() = nil
 
 Base.isempty(lst::Nil) = true
 Base.isempty(lst::Cons) = false
 
-Base.firstindex(lst::List{T}) where T = 1
+Base.firstindex(lst::List) = 1
 
 Base.getindex(lst::Nil, i) = throw(BoundsError(lst, i))
 Base.getindex(lst::Cons, i) = i == 1 ? lst.head : getindex(lst.tail, i-1)
 
-Base.eltype(lst::List{T}) where T = T
+Base.eltype(lst::List{T}) where {T} = T
 
 list(elts...) =
   let l = nil
@@ -57,9 +65,12 @@ list(gen::Base.Generator) =
     iter(iterate(gen))
   end
 
-
-head(x::Cons{T}) where {T} = x.head::T
-tail(x::Cons{T}) where {T} = x.tail
+head(x::Cons{E}) where {E} = x.head::E
+tail(x::Cons{E}) where {E} = x.tail::List{E}
+head(x::Nil) = error("head applied to an empty list")
+tail(x::Nil) = error("tail applied to an empty list")
+head(x::Pair{A,D}) where {A,D} = x.car::A
+tail(x::Pair{A,D}) where {A,D} = x.cdr::D
 
 Base.first(x::Cons) = x.head
 Base.Iterators.drop(x::Cons, n::Integer) = n == 0 ? x : drop(x.tail, n-1)
@@ -69,16 +80,17 @@ Base.iterate(l::List, state::Cons = l) = state.head, state.tail
 import Base.==
 ==(x::Nil, y::Nil) = true
 ==(x::Cons, y::Cons) = (x.head == y.head) && (x.tail == y.tail)
+==(x::Pair, y::Pair) = (x.car == y.car) && (x.cdr == y.cdr)
 
-print_list_type = false
+print_type = false
 
 Base.show(io::IO, lst::Nil) = 
-  print_list_type ? 
+  print_type ? 
     print(io, "List()") :
     print(io, "()")
 Base.show(io::IO, lst::Cons{T}) where T =
   begin
-    print_list_type ?
+    print_type ?
       print(io, "List{$T}(") :
       print(io, "(")
     show_maybe_symbol(io, head(lst))
@@ -93,6 +105,11 @@ show_maybe_symbol(io, e) =
   e isa Symbol ? 
     print(io, e) :
     show(io, e)
+
+Base.show(io::IO, p::Pair{A,D}) where {A,D} = 
+  print_type ?
+    print(io, "Pair{$A,$D}($(p.car) . $(p.cdr))") :
+    print(io, "($(p.car) . $(p.cdr))")
 
 Base.length(l::Nil) = 0
 Base.length(l::Cons) =
