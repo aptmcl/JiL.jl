f2a0975a2c0bc2fe166fae57ea8b22ab1f3715ab
aptmcl <antonio.menezes.leitao@gmail.com>
2024-12-03 10:27:30 +0000

    Added tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,11 @@ using JiL
 using Test
 
 
-
 @testset "JiL.jl" begin
+    @test jil"(def foo 1)" == julia"foo = 1"
+    @test jil"(def (bar x) 1)" == julia"bar(x) = begin 1 end"
+    @test jil"(def (baz x (... y)) 1)" == julia"baz(x, y...) = begin 1 end"
     @test jil"(define foo 1)" == julia"foo = 1"
     @test jil"(define (bar x) 1)" == julia"bar(x) = begin 1 end"
+    @test jil"(define (baz x . y) 1)" == julia"baz(x, y...) = begin 1 end"
 end
