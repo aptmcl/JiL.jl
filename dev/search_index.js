710fb562a26c080735fa82b938485b6c48b2e215
Documenter.jl <documenter@juliadocs.github.io>
2025-07-28 14:52:58 +0000

    build based on b16da9b

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JiL","category":"page"},{"location":"#JiL","page":"Home","title":"JiL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JiL.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JiL]","category":"page"}]
+[{"location":"#JiL","page":"Home","title":"JiL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JiL.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
